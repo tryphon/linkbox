b590a82ec0ea2dff0317f155667720a2df5ffec0
Alban Peignier <alban.peignier@free.fr>
2010-02-20 09:02:33 +0100

    Configure liquidsoap to play the localhost stream or silence

--- manifests/classes/liquidsoap.pp
+++ manifests/classes/liquidsoap.pp
@@ -6,14 +6,11 @@ class liquidsoap {
   file { "/var/lib/liquidsoap":
     ensure => directory
   }
-  file { "/var/lib/liquidsoap/tune.ogg":
-    source => "$source_base/files/liquidsoap/tune.ogg"
+  file { "/var/lib/liquidsoap/stream.liq":
+    source => "$source_base/files/liquidsoap/stream.liq"
   }
-  file { "/var/lib/liquidsoap/test.liq":
-    source => "$source_base/files/liquidsoap/tune.liq"
-  }
-  file { "/etc/liquidsoap/test.liq":
-    ensure => "/var/lib/liquidsoap/test.liq"
+  file { "/etc/liquidsoap/stream.liq":
+    ensure => "/var/lib/liquidsoap/stream.liq"
   }
   exec { "add-liquidsoap-user-to-audio-group":
     command => "adduser liquidsoap audio",
