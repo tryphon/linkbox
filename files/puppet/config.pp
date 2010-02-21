8529353589f58ac63a8b94ef7f6905b805cf7292
Alban Peignier <alban@tryphon.eu>
2010-02-21 21:41:22 +0100

    Add double-quote required by current release of linkcontrol. Refs #1

--- files/puppet/config.pp
+++ files/puppet/config.pp
@@ -1,12 +1,12 @@
-$network_method=dhcp
+$network_method="dhcp"
 $network_static_address="192.168.1.2"
 $network_static_netmask="255.255.255.0"
 $network_static_gateway="192.168.1.1"
 $network_static_dns1="192.168.1.1"
 
-$linkstream_target_host=localhost
-$linkstream_target_port=14100
-$linkstream_udp_port=14100
-$linkstream_http_port=8000
+$linkstream_target_host="localhost"
+$linkstream_target_port="14100"
+$linkstream_udp_port="14100"
+$linkstream_http_port="8000"
 
 
