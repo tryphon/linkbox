d7bca1971defb090d79b52e0903b65c47524d6de
Alban Peignier <alban@tryphon.eu>
2011-06-04 16:13:09 +0200

    Update linkcontrol to 0.4-1lenny1. Manage explicit push/pull modes and configure Release

--- files/linkcontrol/production.rb
+++ files/linkcontrol/production.rb
@@ -29,4 +29,8 @@ config.after_initialize do
   
   # SavePoint.timestamp_file = "/boot/config.pp"
   SavePoint.save_command = "sudo /usr/local/sbin/save-puppet-config"
+
+  Release.latest_url = "http://download.tryphon.eu/linkbox/latest.yml"
+  Release.current_url = "/boot/current.yml"
+  Release.install_command = "sudo /usr/local/sbin/box-upgrade"
 end
