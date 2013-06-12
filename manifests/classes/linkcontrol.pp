ca3b2edec71ad24eb1fc9125d3740068b92e7d59
Alban Peignier <alban@tryphon.eu>
2013-06-13 01:40:25 +0200

    Update linkcontrol to 0.7-2. Fixes #825

--- manifests/classes/linkcontrol.pp
+++ manifests/classes/linkcontrol.pp
@@ -14,7 +14,7 @@ class linkcontrol {
     require => Package[linkcontrol]
   }
   package { linkcontrol:
-    ensure => "0.7-1",
+    ensure => "0.7-2",
     require => [Apt::Source[tryphon], Package[libapache2-mod-passenger]]
   }
 
