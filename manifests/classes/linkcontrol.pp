c15bb03896dafdf693f8fc7665e958af46c3bd81
Alban Peignier <alban@tryphon.eu>
2014-11-25 15:26:11 +0100

    Use linkcontrol 0.9-1+build30 available for i386. Refs #1245

--- manifests/classes/linkcontrol.pp
+++ manifests/classes/linkcontrol.pp
@@ -15,7 +15,7 @@ class linkcontrol {
     require => Package[linkcontrol]
   }
   package { linkcontrol:
-    ensure => "0.9-1+build29",
+    ensure => "0.9-1+build30",
     require => [Apt::Source['tryphon-dev'], Package['libapache2-mod-passenger']]
   }
 
